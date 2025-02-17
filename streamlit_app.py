673b07c651ac894a7f1b2e9a8ac89efbe68ddb8e
Munira-K <kurbanovamunira2003@gmail.com>
2025-02-17 19:04:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,12 @@ with st.expander("Data"):
   st.write("y")
   y_row = df.species
   st.dataframe(y_row)
+
+with st.sidebar:
+  st.header("введите признак:   ")
+  island = st.selectbox("Island", ("Torgersen", "Dream", "Biscoe"))
+  bill_length_mm = st.slider("Bill length", 32.1, 59.6 ,44.5)
+  bill_depth = st.slider("Bill depth", 13.1, 21.5, 17.3)
+  flipper_length = st.slider('Flipper length', 32.1, 59.6 , 44.5)
+  gender = st.slider('Gender', ('female', 'male'))
+  body_mass = st.slider('Body mass', 32.1 , 59.6, 44.5)
