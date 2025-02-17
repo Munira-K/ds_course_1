66c21df381d21805733de2e7de7feba051e9a4c3
Munira-K <kurbanovamunira2003@gmail.com>
2025-02-17 19:31:15 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ st.plotly_chart(fig2)
 data = {
     'island': island,
     'bill_length_mm': bill_length_mm,
-    'bill_depth_mm': bill_depth_mm,
+    'bill_depth_mm': bill_depth,
     'flipper_length_mm': flipper_length_mm,
     'body_mass_g': body_mass_g,
     'sex': gender
