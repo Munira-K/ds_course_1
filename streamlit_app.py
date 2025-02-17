0c518ebf19f41a7a9bad348a946286107d726072
Munira-K <kurbanovamunira2003@gmail.com>
2025-02-17 19:06:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,6 @@ with st.sidebar:
   bill_length_mm = st.slider("Bill length", 32.1, 59.6 ,44.5)
   bill_depth = st.slider("Bill depth", 13.1, 21.5, 17.3)
   flipper_length = st.slider('Flipper length', 32.1, 59.6 , 44.5)
-  gender = st.slider('Gender', ('female', 'male'))
-  body_mass = st.slider('Body mass', 32.1 , 59.6, 44.5)
+  body_mass = st.slider("Body mass", 32.1 , 59.6, 44.5)
+  gender = st.slider("Gender", ('female', 'male'))
+  
