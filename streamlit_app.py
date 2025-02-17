b208893101aebb4bb19dc5e254004add4205eab9
Munira-K <kurbanovamunira2003@gmail.com>
2025-02-17 18:50:10 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Munira's attempts to be smart...")
 
 st.write('So sad...')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander("Data"):
+  st.write("X")
+  X_row = df.drop("species", axis = 1)
+  st.dataframe(X_row)
+
+  st.write("y")
+  y_row = df.species
+  st.dataframe(y_row)
